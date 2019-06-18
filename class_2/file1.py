ae16045840f9ff3d25d5314d7f7d1e02b5770c4a
bittoo aggarwal <bitz1119@gmail.com>
2019-06-18 12:46:48 +0530

    class_1 and class_2

---
+++ class_2/file1.py
@@ -0,0 +1,4 @@
+import module1
+print(module1.sum(1,2))
+
+print(__name__)
---
+++ class_2/module1.py
@@ -0,0 +1,13 @@
+def sum(a,b):
+	print(__name__)
+	return a+b
+
+def diff(a,b):
+	return a-b
+
+print(sum(5,2))
+# if __name__ == '__main__':
+	
+
+
+
